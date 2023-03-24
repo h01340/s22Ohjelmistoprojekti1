0766fb97b48e860e62df9e1179c8c3aa2adac21a
h01340 <minna.pellikka@haaga-helia.fi>
2023-03-24 10:39:55 +0200

    remove security

--- src/main/java/s22/carRest/CarRestApplication.java
+++ src/main/java/s22/carRest/CarRestApplication.java
@@ -43,43 +43,29 @@ public class CarRestApplication implements CommandLineRunner {
 		SpringApplication.run(CarRestApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("https://s22frontend.herokuapp.com/");
-			}
-		};
-	}
-
 	@Override
 	public void run(String... args) throws Exception {
 
-//		log.info("Luodaan muutama omistaja");
-//		Owner owner1 = new Owner("Macy", "Minnie", "Tampere", "150578-223L", 1978);
-//		ownerRepository.save(owner1);
-//		ownerRepository.save(new Owner("John", "Johnson", "Helsinki", "111177-134M", 1977));
-//		ownerRepository.save(new Owner("Kia", "Watson", "Helsinki", "300378-194L", 1978));
-//
-//		// Add car object and link to owners and save these to db
-//		log.info("Luodaan autoja");
-//		carRepository.save(new Car("Volkswagen", "Golf", "White", "Abc-123", 2021, 59000, owner1));
-//		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-112", 2021, 59000,
-//				ownerRepository.findBySsn("111177-134M").get(0)));
-//		carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-300", 2019, 29000,
-//				ownerRepository.findBySsn("111177-134M").get(0)));
-//		carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-212", 2020, 39000,
-//				ownerRepository.findBySsn("300378-194L").get(0)));
-//
-//		log.info("Luodaan kaksi käyttäjää, user ja admin");
-//		//user, user & admin, admin
-//		applicationUserRepository.save(new ApplicationUser ("user", "$2a$10$gYhRVPCVkxonw4C6KbzBHOGfN3tR0cq8S51Ojct3iKEtP7fBCeZke", "USER"));
-//		applicationUserRepository.save(new ApplicationUser ("admin", "$2a$04$g6UsslKArS7jZrevnIpQIe5udQCOSriLtEz19jDRFUVPM74FT2rvG", "ADMIN"));
-		log.info("Print users");
-		for (ApplicationUser user : applicationUserRepository.findAll()) {
-			System.out.println(user.toString());
-		}
+		log.info("Luodaan muutama omistaja");
+		Owner owner1 = new Owner("Macy", "Minnie", "Tampere", "150578-223L", 1978);
+		ownerRepository.save(owner1);
+		ownerRepository.save(new Owner("John", "Johnson", "Helsinki", "111177-134M",
+				1977));
+		ownerRepository.save(new Owner("Kia", "Watson", "Helsinki", "300378-194L",
+				1978));
+
+		// Add car object and link to owners and save these to db
+		log.info("Luodaan autoja");
+		carRepository.save(new Car("Volkswagen", "Golf", "White", "Abc-123", 2021,
+				59000, owner1));
+		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-112", 2021, 59000,
+				ownerRepository.findBySsn("111177-134M").get(0)));
+		carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-300", 2019, 29000,
+				ownerRepository.findBySsn("111177-134M").get(0)));
+		carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-212", 2020,
+				39000,
+				ownerRepository.findBySsn("300378-194L").get(0)));
+
 		log.info("Tulostetaan IDEn konsoliin autot omistajineen:");
 		for (Car car : carRepository.findAll()) {
 			log.info(car.toString());
